4b90840966e3d4a54708d57936ceadcb219b38f8
johnrnyquist <john@nyquist.net>
2024-09-27 13:27:29 -0600

    Adding Display

--- Ecsity/Components.swift
+++ Ecsity/Components.swift
@@ -1,9 +1,22 @@
 import Foundation
 
-
 public class Position: Component, CustomStringConvertible, Equatable {
     public var x: Double
     public var y: Double
+    public var point: CGPoint {
+        get {
+            CGPoint(x: x, y: y)
+        }
+        set {
+            x = Double(newValue.x)
+            y = Double(newValue.y)
+        }
+    }
+
+    public init(point: CGPoint) {
+        x = Double(point.x)
+        y = Double(point.y)
+    }
 
     public init(x: Double, y: Double) {
         self.x = x
@@ -15,7 +28,7 @@ public class Position: Component, CustomStringConvertible, Equatable {
     }
 
     public static func ==(lhs: Position, rhs: Position) -> Bool {
-        return lhs.x == rhs.x && lhs.y == rhs.y
+        lhs.x == rhs.x && lhs.y == rhs.y
     }
 }
 
@@ -26,5 +39,23 @@ public struct Velocity: Component {
     public init(dx: Double, dy: Double) {
         self.dx = dx
         self.dy = dy
+    }
+}
+
+import SpriteKit
+
+class Display: Component, Hashable, Equatable {
+    var node: SKNode
+
+    init(node: SKNode) {
+        self.node = node
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+
+    static func ==(lhs: Display, rhs: Display) -> Bool {
+        ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
     }
 }
