ae877463714aeb870f54605954e2ca55e82bf3b1
johnrnyquist <john@nyquist.net>
2024-09-27 10:10:21 -0600

    Updating access

--- Ecsity/Engine.swift
+++ Ecsity/Engine.swift
@@ -9,21 +9,21 @@ open class Engine {
         storage = ArchetypeStorage()
     }
 
-    func add(system: System) {
+    public func add(system: System) {
         systems.append(system)
     }
 
-    func update(deltaTime: TimeInterval) {
+    public func update(deltaTime: TimeInterval) {
         for system in systems {
             system.update(deltaTime: deltaTime)
         }
     }
 
-    func remove(entity: Entity) {
+    public func remove(entity: Entity) {
         storage.remove(entity: entity)
     }
     
-    func remove(componentType: Component.Type, from entity: Entity) {
+    public func remove(componentType: Component.Type, from entity: Entity) {
         storage.remove(componentType: componentType, from: entity)
     }
 }
